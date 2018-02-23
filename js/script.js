8b9d86533e0f3fb5ce7f35d5129c52fcf6ee715b
jessepackwood <packwoodjesse@gmail.com>
2018-02-23 15:12:26 -0700

    responsive header bar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const showMobileTab = (event) => {
 $(event.target).toggleClass('active')
 $(event.target).next().toggleClass('mobile-hide')
-  // console.log(event.target)
+}
 
-// $('.card-info').removeClass('mobile-hide')
+const showMobileHeader = () => {
+  console.log(event.target)
+  $('nav').find('.nav-link-container').toggleClass('nav-hidden')
 }
 
 const showTab = (event) => {
@@ -11,10 +13,9 @@ const showTab = (event) => {
   $(event.target).addClass('active')
   $('.card').addClass('hide')
   $(`.card-${$(event.target).attr('id')}`).removeClass('hide')
-  // $(`${$(event.target).attr('id')}`).removeClass('hide')
 }
 
 
 $('.tab').on('click', showTab)
 $('.mobile-tab').on('click', showMobileTab)
-// $('.view-code').on('click', showCode)
+$('.menu-button').on('click', showMobileHeader)
